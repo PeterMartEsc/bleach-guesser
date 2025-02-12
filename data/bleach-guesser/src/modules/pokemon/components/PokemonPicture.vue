88978e0da499541e5a3e389b69da3740bfcd6e0f
PeterMartEsc <petermartesc@gmail.com>
2025-02-12 19:18:48 +0000

    feat(data): Se copia un proyecto base para trabajar a partir de el

---
+++ data/bleach-guesser/src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="flex flex-col justify-center items-center">
+    <img v-if="!showPokemon" :src="pokemonImage" class="brightness-0 h-[200px]" />
+    <img v-else :src="pokemonImage" class="h-[200px]" alt="pokemon image"/>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+interface Props{
+  pokemonId: number;
+  showPokemon?: boolean;
+}
+
+//const props = defineProps<Props>();
+
+const pokemonImage = computed(
+  ()=> `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.pokemonId}.png`
+)
+
+const props = withDefaults( defineProps<Props>(), {
+  showPokemon: false,
+});
+
+</script>
+
+<style scoped>
+
+</style>
