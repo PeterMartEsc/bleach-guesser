d94ce20db0ae82c078b67b8d76739be304b84454
ATPRodriguez <alejandrotomaspachecorodriguez@gmail.com>
2025-02-18 13:54:58 +0000

    feat: Agregado componente GuessManga.vue

---
+++ data/bleach-guesser/src/components/GuessManga.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+      <header class="header">
+            <div class="bg-[#ccc]">
+            <h1 class="text-black">Anime Guesser</h1>
+            </div>
+        </header>
+        <br>
+        <div v-if="titles" class="character-container">
+            <img :src="images" class="character-image"/>
+        </div>
+        <p v-else>Cargando personajes...</p>
+        <div class="input-container">
+          <input 
+            v-model="userGuess" 
+            placeholder="Escribe el nombre..." 
+            @keyup.enter="checkGuess"
+          />
+        </div>
+        <br>    
+        <button @click="checkGuess" class="bg-[#ccc] text-black">Adivinar</button>
+        <p :class="{ correct: message.includes('Correcto'), incorrect: message.includes('Incorrecto') }">
+          {{ message }}
+        </p>
+    </div>
+</template>
+  
+    <script setup>
+    import { ref, onMounted, computed } from "vue";
+    import "@/assets/guesser.css";
+  
+    const apiUrl = "https://api.jikan.moe/v4/manga/";
+    const images = ref("");
+    const titles = ref("");
+    
+    const userGuess = ref("");
+    const message = ref("");
+    const showSuggestions = ref(false);
+  
+    async function fetchRandomAnime() {
+      try {
+        const randomIndex = Math.floor(Math.random() * 10000);
+        const newUrl = apiUrl + randomIndex;
+        const response = await fetch(newUrl);
+        const data = await response.json();
+        titles.value = data.data.title;
+        images.value = data.data.images.jpg.large_image_url;
+
+        message.value = "";
+        userGuess.value = "";
+        console.log(titles.value)
+      } catch (error) {
+        console.error("Error fetching characters:", error);
+      }
+    }
+  
+    function checkGuess() {
+      if (!titles.value) return;
+      const correctName = titles.value.toLowerCase();
+      if (userGuess.value.toLowerCase() === correctName) {
+        message.value = "¡Correcto! 🎉";
+        setTimeout(fetchRandomAnime, 2000);
+      } else {
+        message.value = "Incorrecto, intenta otra vez. ❌";
+      }
+      showSuggestions.value = false;
+    }
+  
+  /**   const filteredSuggestions = computed(() => {
+      if (!userGuess.value) return [];
+      return characters.value
+        .map((char) => char.character.name)
+        .filter((name) => name.toLowerCase().includes(userGuess.value.toLowerCase()))
+        .slice(0, 5);
+    });
+  
+    function selectSuggestion(name) {
+      userGuess.value = name;
+      showSuggestions.value = false;
+    }
+  */
+    onMounted(fetchRandomAnime);
+    </script>
+  
+    <style scoped>
+  
+    </style>
+  
